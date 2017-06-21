5cfc5e4aa08100632a8c2cbfca02ef5dd7ca4aa0
yysskk <yusuke.0213@gmail.com>
2017-06-21 17:18:12 +0900

    Initial commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 森下 侑亮 on 2017/06/21.
+//  Copyright © 2017年 yysskk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sources/Supporting files/SwipeMenuViewController.h
@@ -0,0 +1,19 @@
+//
+//  SwipeMenuViewController.h
+//  SwipeMenuViewController
+//
+//  Created by 森下 侑亮 on 2017/06/21.
+//  Copyright © 2017年 yysskk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeMenuViewController.
+FOUNDATION_EXPORT double SwipeMenuViewControllerVersionNumber;
+
+//! Project version string for SwipeMenuViewController.
+FOUNDATION_EXPORT const unsigned char SwipeMenuViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeMenuViewController/PublicHeader.h>
+
+
