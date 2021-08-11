461c43a4678e04165dddbf005357178e7ba14ad9
Maxim Mamedov <mmd.developer@gmail.com>
2021-08-11 23:43:01 +0300

    Update TabView.swift

--- Sources/Classes/TabView.swift
+++ Sources/Classes/TabView.swift
@@ -352,6 +352,7 @@ extension TabView {
         case .underline:
             let itemView = itemViews[currentIndex]
             additionView = UIView(frame: CGRect(x: itemView.frame.origin.x + options.additionView.padding.left, y: itemView.frame.height - options.additionView.padding.vertical, width: itemView.frame.width - options.additionView.padding.horizontal, height: options.additionView.underline.height))
+            additionView.layer.cornerRadius = options.additionView.underline.height / 2
             additionView.backgroundColor = options.additionView.backgroundColor
             containerView.addSubview(additionView)
         case .circle:
